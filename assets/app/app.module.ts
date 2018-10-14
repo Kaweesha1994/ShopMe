5cba3b035cc292d9cc1766c2f09c57dd0b244047
kaweesha hasarinda <kaweeshasharinda@gmail.com>
2018-10-14 14:14:35 -0400

    added new module common for the common components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import {CommonModule} from "./common/common.module.ts";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, CommonModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
