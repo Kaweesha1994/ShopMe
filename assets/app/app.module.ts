5ae029ba151a203e7e9c0fe5063d5984ca02ac73
kaweesha hasarinda <kaweeshasharinda@gmail.com>
2018-10-18 11:48:27 -0400

    edit issue

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
-import {CommonModule} from "./common/common.module.ts";
+import {CommonModule} from "./common/common.module";
 import { IndexComponent } from "./index/index.component";
-import {AppRoutingModule} from "./app-routing.module.ts";
+import {AppRoutingModule} from "./app-routing.module";
 import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
