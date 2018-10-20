062e6b043e9a180acf8980c5b28077d90d5d6ded
kaweesha hasarinda <kaweeshasharinda@gmail.com>
2018-10-20 08:59:08 -0400

    added font awesome from bootstrap

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -7,13 +7,19 @@ import { IndexComponent } from "./component/index/index.component";
 import {AppRoutingModule} from "./app-routing.module";
 import { ContactComponent } from "./component/contact/contact.component";
 import { FeatureComponent } from './component/feature/feature.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
-    declarations: [AppComponent, 
-    			   IndexComponent,
-                   ContactComponent,
-                   FeatureComponent],
-    imports: [BrowserModule, CommonModule, AppRoutingModule],
+    declarations: [
+        AppComponent, 
+        IndexComponent,
+        ContactComponent,
+        FeatureComponent],
+    imports: [
+        BrowserModule, 
+        CommonModule, 
+        AppRoutingModule,
+        AngularFontAwesomeModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
